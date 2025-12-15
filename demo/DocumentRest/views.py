286f2ddf9fae8a3991a9ae5ce199b8aa97cd6afa
Eder Alcazar <ederalcazar@gmail.com>
2025-12-15 14:21:45 -0300

    Url functioning Example

--- demo/DocumentRest/views.py
+++ demo/DocumentRest/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+
+# Create your models here.
+def home(request):
+    # return HttpResponse('Hello World!')
+    return render(request, 'home.html')
 
 # Create your views here.
